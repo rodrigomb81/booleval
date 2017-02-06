8ee4f9a0db169a6051c7deb5539987d7f9e24127
Rodrigo Maximiliano Bellusci <rodrigo_bellusci@outlook.com>
2017-02-06 20:28:16 -0300

    agregar funciones para manipular la lista leida...
    Estas funciones son `arbol` y `rpn`. La primera toma una expresion leida y crea un arbol binario que la representa. Detecta errores en la expresion y los pasa para arriba. La segunda toma el arbol y lo convierte en una expresion rpn que se puede evaluar correctamente. Tambien pasa para arriba los errores encontrados por los pasos anteriores (leer y arbol).

--- index.ts
+++ index.ts
@@ -14,11 +14,86 @@ export interface Token {
     nombre: string
     texto: string
     tipo: 'var' | 'valor' | 'conectivo' | 'parentesis'
+    pos: number
 }
 
-export interface CaracterInesperado {
-    pos?: number
+export interface ErrorMultiple {
+    nombre: 'ErrorMultiple'
+    errores: (CaracterInesperado | ParentesisAbierto)[]
+}
+
+export type ErrorPosible = ParentesisAbierto | ParentesisExtraviado | CaracterInesperado
+
+export interface ErrorBase {
+    nombre: string
+    pos: number
+}
+
+export interface ParentesisExtraviado extends ErrorBase {
+    nombre: 'ParentesisExtraviado'
+}
+
+export interface CaracterInesperado extends ErrorBase {
+    nombre: 'CaracterInesperado'
     caracter: string
+}
+
+export interface ParentesisAbierto extends ErrorBase {
+    nombre: 'ParentesisAbierto'
+    // pos: el indice del parentesis que falta cerrar
+}
+
+export interface ExpresionLeida {
+    pos: number
+    tokens: List<Token>
+    vars: List<String>
+}
+
+export class Nodo<A> {
+    private _contenido: A
+    private _izquierda: Nodo<A>
+    private _derecha: Nodo<A>
+    private contenido_set: boolean
+
+    constructor() {
+        this.contenido_set = false
+        this._izquierda = null
+        this._derecha = null
+    }
+
+    set contenido(c: A) {
+        /**
+         * Por las dudas agrego esto para detectar bugs donde se cambia el contenido de un nodo.
+         * Una vez que las pruebas pasen se puede borrar.
+         */
+        if (!this.contenido_set) {
+            this._contenido = c
+            this.contenido_set = true
+        }
+        else {
+            throw new Error('El contenido de este nodo ya fue establecido')
+        }
+    }
+
+    get contenido(): A {
+        return this._contenido
+    }
+
+    set izquierda(i: Nodo<A>) {
+        this._izquierda = i
+    }
+
+    get izquierda(): Nodo<A> {
+        return this._izquierda
+    }
+
+    set derecha(d: Nodo<A>) {
+        this._derecha = d
+    }
+
+    get derecha(): Nodo<A> {
+        return this._derecha
+    }
 }
 
 /**
@@ -36,27 +111,237 @@ function evaluar(expresion: List<Token>, stack: List<boolean>): List<boolean> {
 }
 
 /**
- * rpn$: envoltura para rpn que recorre el arbol en orden para generar una expresion rpn...tal vez no sea necesaria
+ * rpn: envoltura para `aplastar`, devuelve el error encontrado o una lista de tokens que se puede evaluar facilmente
  */
 
+export function rpn(expresion_maybe: Fallo<CaracterInesperado> | Exito<ExpresionLeida>): Fallo<ErrorPosible> | Exito<List<Token>> {
+    if (expresion_maybe.error) {
+        return expresion_maybe
+    }
+    else {
+        const tokens = (expresion_maybe.resultado as ExpresionLeida).tokens
+        const arbol_maybe = arbol(tokens, {error: false, resultado: new Nodo<Token>()})
+        if (arbol_maybe.error) {
+            return arbol_maybe
+        }
+        else {
+            return {error: false, resultado: List(aplastar(arbol_maybe.resultado as Nodo<Token>))}
+        }
+    }
+}
+
+function aplastar<A>(nodo: Nodo<A>): A[] {
+    let izquierda: A[] = []
+    let derecha: A[] = []
+
+    if (nodo.izquierda != null) {
+        izquierda = aplastar(nodo.izquierda)
+    }
+
+    if (nodo.derecha != null) {
+        derecha = aplastar(nodo.derecha)
+    }
+
+    return [...izquierda, ...derecha, nodo.contenido]
+}
+
 /**
- * rpn: reordena una lista de simbolos para convertirlos en una expresion RPN
+ * arbol_optimista: crea un arbol a partir de una expresion asumiendo que es CORRECTA (no tiene errores)
  */
-function rpn(expresion: List<Token>): List<Token> {
-    return List([])
+export function arbol_optimista(expresion: List<Token>, raiz: Nodo<Token>): Nodo<Token> {
+    if (expresion.size > 0) {
+        let tokens: List<Token> = null
+
+        if (expresion.get(0).tipo == 'parentesis') {
+            // remover parentesis al principio y al final
+            tokens = List(expresion.toArray().slice(1, buscarPareja(expresion)))
+        }
+        else {
+            tokens = expresion
+        }
+
+        const indice = buscarConectivo(tokens).resultado as number
+
+        if (indice != -1 ) {
+            raiz.contenido = tokens.get(indice)
+            const mitad_izq = List(tokens.toArray().slice(0, indice))
+            const mitad_der = List(tokens.toArray().slice(indice + 1))
+            raiz.izquierda = arbol_optimista(mitad_izq, new Nodo<Token>())
+            raiz.derecha = arbol_optimista(mitad_der, new Nodo<Token>())
+            return raiz
+        }
+        else {
+            raiz.contenido = tokens.get(0)
+            raiz.izquierda = null
+            raiz.derecha = null
+            return raiz
+        }
+    }
+    else {
+        return null
+    }
+}
+
+/**
+ * arbol: crea un arbol a partir de una expresion. Este arbol puede ser "aplastado" para obtener una lista de tokens
+ * que se puede evaluar como expresion rpn, o puede evaluarse directamente.
+ */
+export function arbol(expresion: List<Token>, raiz_maybe: Fallo<ErrorPosible> | Exito<Nodo<Token>>): Fallo<ErrorPosible> | Exito<Nodo<Token>> {
+    if (!raiz_maybe.error) {
+        const raiz = raiz_maybe.resultado as Nodo<Token>
+        if (expresion.size > 0) {
+            let tokens: List<Token> = null
+
+            // remover parentesis si es necesario y manejar errores posibles
+            if (expresion.get(0).tipo == 'parentesis') {
+                if (expresion.get(0).nombre == 'par-izquierdo') {
+                    const indice_pareja = buscarPareja(List(expresion))
+                    if (indice_pareja != -1) {
+                        tokens = List(expresion.toArray().slice(1, indice_pareja))
+                    }
+                    else {
+                        return {error: true, resultado: {nombre: 'ParentesisAbierto', pos: expresion.get(0).pos}}
+                    }
+                }
+                else {
+                    return {error: true, resultado: {nombre: 'ParentesisExtraviado', pos: expresion.get(0).pos}}
+                }
+            }
+            else {
+                tokens = expresion
+            }
+
+            // indice del conectivo que sera el contenido de este nodo
+            const indice_maybe = buscarConectivo(List(tokens))
+
+            if (!indice_maybe.error) {
+                const indice = indice_maybe.resultado as number
+
+                if (indice != -1) {
+
+                    // si hay conectivo, usarlo como contenido de la raiz y generar las ramas
+                    raiz.contenido = tokens.get(indice)
+
+                    const mitad_izq = List(tokens.toArray().slice(0, indice))
+                    const rama_izq_maybe = arbol(mitad_izq, {error: false, resultado: new Nodo<Token>()})
+
+                    const mitad_der = List(tokens.toArray().slice(indice + 1))
+                    const rama_der_maybe = arbol(mitad_der, {error: false, resultado: new Nodo<Token>()})
+
+                    if (rama_izq_maybe.error == false && rama_der_maybe.error == false) {
+                        // si llegamos aca, todo salió bien
+                        raiz.izquierda = rama_izq_maybe.resultado as Nodo<Token>
+                        raiz.derecha = rama_der_maybe.resultado as Nodo<Token>
+
+                        return {error: false, resultado: raiz}
+                    }
+                    else {
+                        // devolver el error encontrado, dandole prioridad al error de la rama izquierda
+                        return rama_izq_maybe.error ? rama_izq_maybe:rama_der_maybe
+                    }
+                }
+                else {
+                    // no hay conectivo, este nodo solo contiene una variable y no tiene ramas
+                    raiz.contenido = tokens.get(0)
+                    raiz.izquierda = null
+                    raiz.derecha = null
+                    return {error: false, resultado: raiz}
+                }
+            }
+            else {
+                return indice_maybe
+            }
+        }
+        else {
+            return null
+        }
+    }
+    else {
+        return raiz_maybe
+    }
+}
+
+/**
+ * buscarConectivo devuelve el indice del conectivo de mayor precedencia de la expresion. Si la
+ * expresion no tiene conectivos, devuelve -1. Si el conectivo de mayor precedencia se repite,
+ * devuelve el indice del primero. Los conectivos dentro de parentesis son salteados.
+ */
+export function buscarConectivo(expresion: List<Token>): Fallo<ErrorPosible> | Exito<number> {
+    let indice = 0;
+    let precedencia_max = -1;
+
+    for (let i = 0; i < expresion.size; i++) {
+        let simbolo = expresion.get(i)
+        if (simbolo.tipo == 'parentesis') {
+            if (simbolo.nombre == 'par-derecho') {
+                return {error: true, resultado: {nombre: 'ParentesisExtraviado', pos: simbolo.pos}}
+            }
+            else {
+                const indice_pareja = buscarPareja(expresion, i)
+
+                if (indice_pareja == -1) {
+                    return {error: true, resultado: {nombre: 'ParentesisAbierto', pos: simbolo.pos}}
+                }
+                else {
+                    i = indice_pareja
+                }
+            }
+        }
+        else if (simbolo.tipo == 'conectivo') {
+            if (conectivos_precedencia[simbolo.texto] > precedencia_max) {
+                indice = i
+                precedencia_max = conectivos_precedencia[simbolo.texto]
+            }
+        }
+    }
+
+    if (precedencia_max == -1) {
+        // esto significa que no se encontro NINGUN conectivo en la expresion
+        return {error: false, resultado: -1}
+    }
+    else {
+        return {error: false, resultado: indice}
+    }
+}
+
+export function buscarPareja(expresion: List<Token>, desde: number = 0): number {
+    /**
+     * Indice de la pareja
+     */
+    let indice = desde + 1
+    let pars_abiertos = 1
+    while (indice < expresion.size && pars_abiertos > 0) {
+        const token = expresion.get(indice)
+        if (token.nombre == 'par-izquierdo') {
+            pars_abiertos++
+        }
+        else if (token.nombre == 'par-derecho') {
+            pars_abiertos--
+        }
+        if (pars_abiertos > 0) {
+            indice++
+        }
+    }
+
+    if (pars_abiertos > 0) {
+        return -1
+    }
+    else {
+        return indice
+    }
 }
 
 /**
  * leer: envoltura para leer$...solo sirve para la primer invocacion
  */
-export function leer(expresion: string): Fallo<CaracterInesperado> | Exito<List<Token>> {
-    return leer$(expresion, {error: false, resultado: List([])})
+export function leer(expresion: string): Fallo<CaracterInesperado> | Exito<ExpresionLeida> {
+    return leer$(expresion, {error: false, resultado: {pos: 0, tokens: List([]), vars: List([])}})
 }
 
 /**
  * leer$ reduce una cadena a una lista de simbolos (variables y conectivos)
  */
-export function leer$(expresion: string, maybe_tokens: Fallo<CaracterInesperado> | Exito<List<Token>>): Fallo<CaracterInesperado> | Exito<List<Token>> {
+export function leer$(expresion: string, exp_maybe: Fallo<CaracterInesperado> | Exito<ExpresionLeida>): Fallo<CaracterInesperado> | Exito<ExpresionLeida> {
     /**
      * cosas que pueden aparecer al leer una expresion:
      * una palabra (una variable)
@@ -70,45 +355,49 @@ export function leer$(expresion: string, maybe_tokens: Fallo<CaracterInesperado>
     const valor = /^[TC]/
     const espacio_en_blanco = /^\s+/
 
-    if (expresion.length > 0 || maybe_tokens.error)  {
+    if (expresion.length > 0 && !exp_maybe.error)  {
         // si llegamos aca todavia quedan simbolos en la cadena y no hubo errores
-        const tokens = maybe_tokens.resultado as List<Token>
+        const exp = exp_maybe.resultado as ExpresionLeida
 
         if (palabra.test(expresion)) {
             // extraer la palabra
             const texto = expresion.match(palabra)[0]
-            const token: Token = {nombre: texto, texto, tipo: 'var'}
-            return leer$(expresion.substr(texto.length), {error: false, resultado: tokens.push(token)})
+            const token: Token = {nombre: texto, texto, tipo: 'var', pos: exp.pos}
+            const pos = exp.pos + texto.length
+            return leer$(expresion.substr(texto.length), {error: false, resultado: {pos, tokens: exp.tokens.push(token), vars: exp.vars.push(texto)}})
         }
         else if (conectivo.test(expresion)) {
             // extraer el conectivo
             const texto = expresion.match(conectivo)[0]
             const nombre = conectivos_nombres[texto]
-            return leer$(expresion.substr(1), {error: false, resultado: tokens.push({nombre, texto, tipo: 'conectivo'})})
+            const token: Token = {nombre, texto, tipo: 'conectivo', pos: exp.pos}
+            return leer$(expresion.substr(1), {error: false, resultado: {pos: exp.pos + 1, tokens: exp.tokens.push(token), vars: exp.vars}})
         }
         else if (parens.test(expresion)) {
             // extraer el parentesis
             const texto = expresion.match(parens)[0]
             const nombre = texto == ')' ? 'par-derecho':'par-izquierdo'
-            return leer$(expresion.substr(1), {error: false, resultado: tokens.push({nombre, texto, tipo: 'parentesis'})})
+            const token: Token = {nombre, texto, tipo: 'parentesis', pos: exp.pos}
+            return leer$(expresion.substr(1), {error: false, resultado: {pos: exp.pos + 1, tokens: exp.tokens.push(token), vars: exp.vars}})
         }
         else if (valor.test(expresion)) {
             // extraer el valor
             const texto = expresion.match(valor)[0]
             const nombre = texto == 'T' ? 'tautologia':'contradiccion'
-            return leer$(expresion.substr(1), {error: false, resultado: tokens.push({nombre, texto, tipo: 'valor'})})
+            const token: Token = {nombre, texto, tipo: 'valor', pos: exp.pos}
+            return leer$(expresion.substr(1), {error: false, resultado: {pos: exp.pos + 1, tokens: exp.tokens.push(token), vars: exp.vars}})
         }
         else if (espacio_en_blanco.test(expresion)) {
             const espacio = expresion.match(espacio_en_blanco)
-            return leer$(expresion.substr(espacio.length), maybe_tokens)
+            return leer$(expresion.substr(espacio.length), {error: false, resultado: {pos: exp.pos + espacio.length, tokens: exp.tokens, vars: exp.vars}})
         }
         else {
             // nos encontramos con un valor inesperado (un numero, por ejemplo)
-            return {error: true, resultado: {caracter: expresion[1]}}
+            return {error: true, resultado: {nombre: 'CaracterInesperado', pos: exp.pos, caracter: expresion[1]}}
         }
     }
     else {
-        return maybe_tokens
+        return exp_maybe
     }
 }
 
@@ -118,4 +407,12 @@ const conectivos_nombres: {[nombre: string]: string} = {
     '!': 'complemento'  ,
     '>': 'implicacion'  ,
     '=': 'equivalencia'
+}
+
+const conectivos_precedencia: {[nombre: string]: number} = {
+    '&': 2,
+    '|': 1,
+    '!': 0,
+    '>': 3,
+    '=': 4,
 }
--- test/pruebas.ts
+++ test/pruebas.ts
@@ -1,27 +1,28 @@
 import 'mocha'
 import 'should'
 
-import {leer, Token, CaracterInesperado} from '../index'
+import {leer, Token, CaracterInesperado, ExpresionLeida, buscarPareja, buscarConectivo} from '../index'
+import {Nodo, arbol, arbol_optimista, rpn} from '../index'
 import {List} from 'immutable'
 
 describe('Toda clase de pruebas:', () => {
     describe('leer:', () => {
         it('todos los simbolos', () => {
-            const e = '! > = | & variable T C ()' 
+            const e = '!>=|&vTC()' 
             const tokens_maybe = leer(e)
 
             tokens_maybe.error.should.equal(false)
-            const tokens = (tokens_maybe.resultado as List<Token>).toArray()
-            tokens[0].should.deepEqual({nombre: 'complemento', texto: '!', tipo: 'conectivo'})
-            tokens[1].should.deepEqual({nombre: 'implicacion', texto: '>', tipo: 'conectivo'})
-            tokens[2].should.deepEqual({nombre: 'equivalencia', texto: '=', tipo: 'conectivo'})
-            tokens[3].should.deepEqual({nombre: 'disyuncion', texto: '|', tipo: 'conectivo'})
-            tokens[4].should.deepEqual({nombre: 'conjuncion', texto: '&', tipo: 'conectivo'})
-            tokens[5].should.deepEqual({nombre: 'variable', texto: 'variable', tipo: 'var'})
-            tokens[6].should.deepEqual({nombre: 'tautologia', texto: 'T', tipo: 'valor'})
-            tokens[7].should.deepEqual({nombre: 'contradiccion', texto: 'C', tipo: 'valor'})
-            tokens[8].should.deepEqual({nombre: 'par-izquierdo', texto: '(', tipo: 'parentesis'})
-            tokens[9].should.deepEqual({nombre: 'par-derecho', texto: ')', tipo: 'parentesis'})
+            const tokens = (tokens_maybe.resultado as ExpresionLeida).tokens.toArray()
+            tokens[0].should.deepEqual({nombre: 'complemento', texto: '!', tipo: 'conectivo', pos: 0})
+            tokens[1].should.deepEqual({nombre: 'implicacion', texto: '>', tipo: 'conectivo', pos: 1})
+            tokens[2].should.deepEqual({nombre: 'equivalencia', texto: '=', tipo: 'conectivo', pos: 2})
+            tokens[3].should.deepEqual({nombre: 'disyuncion', texto: '|', tipo: 'conectivo', pos: 3})
+            tokens[4].should.deepEqual({nombre: 'conjuncion', texto: '&', tipo: 'conectivo', pos: 4})
+            tokens[5].should.deepEqual({nombre: 'v', texto: 'v', tipo: 'var', pos: 5})
+            tokens[6].should.deepEqual({nombre: 'tautologia', texto: 'T', tipo: 'valor', pos: 6})
+            tokens[7].should.deepEqual({nombre: 'contradiccion', texto: 'C', tipo: 'valor', pos: 7})
+            tokens[8].should.deepEqual({nombre: 'par-izquierdo', texto: '(', tipo: 'parentesis', pos: 8})
+            tokens[9].should.deepEqual({nombre: 'par-derecho', texto: ')', tipo: 'parentesis', pos: 9})
         })
         
         it('expresion sin errores', () => {
@@ -29,10 +30,10 @@ describe('Toda clase de pruebas:', () => {
             const tokens_maybe = leer(e)
 
             tokens_maybe.error.should.equal(false)
-            const tokens = (tokens_maybe.resultado as List<Token>).toArray()
-            tokens[0].should.deepEqual({nombre: 'una', texto: 'una', tipo: 'var'})
-            tokens[1].should.deepEqual({nombre: 'disyuncion', texto: '|', tipo: 'conectivo'})
-            tokens[2].should.deepEqual({nombre: 'otra', texto: 'otra', tipo: 'var'})
+            const tokens = (tokens_maybe.resultado as ExpresionLeida).tokens.toArray()
+            tokens[0].should.deepEqual({nombre: 'una', texto: 'una', tipo: 'var', pos: 0})
+            tokens[1].should.deepEqual({nombre: 'disyuncion', texto: '|', tipo: 'conectivo', pos: 4})
+            tokens[2].should.deepEqual({nombre: 'otra', texto: 'otra', tipo: 'var', pos: 6})
         })
 
         it('expresion con un error', () => {
@@ -44,4 +45,136 @@ describe('Toda clase de pruebas:', () => {
             error.caracter.should.equal('2')
         })
     })
+
+  describe('buscarPareja:', () => {
+      it('encuentra la pareja buscada', () => {
+          const tokens = (leer('(hola)').resultado as ExpresionLeida).tokens
+          const indice = buscarPareja(tokens)
+          indice.should.equal(2)
+      })
+
+      it('devuelve -1 cuando hay un parentesis abierto', () => {
+          const tokens = (leer('(hola').resultado as ExpresionLeida).tokens
+          const indice = buscarPareja(tokens)
+          indice.should.equal(-1)
+      })
+  })
+
+  describe('buscarConectivo', () => {
+      it('encuentra el conectivo', () => {
+          const tokens = (leer('a | c & b').resultado as ExpresionLeida).tokens
+          const indice_maybe = buscarConectivo(tokens)
+          indice_maybe.error.should.equal(false)
+          indice_maybe.resultado.should.equal(3)
+      })
+
+      it('encuentra el conectivo de nuevo', () => {
+          const tokens = (leer('(a = c) & b').resultado as ExpresionLeida).tokens
+          const indice_maybe = buscarConectivo(tokens)
+          indice_maybe.error.should.equal(false)
+          indice_maybe.resultado.should.equal(5)
+      })
+
+      it('ignora conectivos entre parentesis', () => {
+          const tokens = (leer('a | (a & b)').resultado as ExpresionLeida).tokens
+          const indice_maybe = buscarConectivo(tokens)
+          indice_maybe.error.should.equal(false)
+          indice_maybe.resultado.should.equal(1)
+      })
+
+      it('detecta un parentesis abierto', () => {
+          const tokens = (leer('(a | c & b').resultado as ExpresionLeida).tokens
+          const indice_maybe = buscarConectivo(tokens)
+          indice_maybe.error.should.equal(true)
+          indice_maybe.resultado.should.deepEqual({nombre: 'ParentesisAbierto', pos: 0})
+      })
+
+      it('devuelve -1 cuando no encuentra nada', () => {
+          const tokens = (leer('a').resultado as ExpresionLeida).tokens
+          const indice_maybe = buscarConectivo(tokens)
+          indice_maybe.error.should.equal(false)
+          indice_maybe.resultado.should.equal(-1)
+      })
+
+      it('remueve una pareja de parentesis', () => {
+          const expresion =  (leer('(a&b)').resultado as ExpresionLeida).tokens
+          const sin_parentesis = List(expresion.toArray().slice(1, buscarPareja(expresion)))
+          sin_parentesis.get(0).should.deepEqual({tipo: 'var', nombre: 'a', texto: 'a', pos: 1})
+          sin_parentesis.get(2).should.deepEqual({tipo: 'var', nombre: 'b', texto: 'b', pos: 3})
+      })
+  })
+
+  describe('arbol', () => {
+      it('optimista', () => {
+          const tokens = (leer('(a&b)').resultado as ExpresionLeida).tokens
+          const arbol = arbol_optimista(tokens, new Nodo<Token>())
+          arbol.contenido.should.equal(tokens.get(2))
+          
+          arbol.izquierda.contenido.should.equal(tokens.get(1))
+          should.assert(arbol.izquierda.izquierda == null, 'La rama izquierda no es null')
+          should.assert(arbol.izquierda.derecha == null, 'La rama derecha no es null')
+
+          arbol.derecha.contenido.should.equal(tokens.get(3))
+          should.assert(arbol.derecha.izquierda == null, 'La rama izquierda no es null')
+          should.assert(arbol.derecha.derecha == null, 'La rama derecha no es null')
+      })
+
+      it('V2', () => {
+          const tokens = (leer('(a&b)').resultado as ExpresionLeida).tokens
+          const arbol_maybe = arbol(tokens, {error: false, resultado: new Nodo<Token>()})
+
+          arbol_maybe.error.should.equal(false)
+
+          const raiz = arbol_maybe.resultado as Nodo<Token>
+
+          raiz.contenido.should.equal(tokens.get(2))
+          
+          raiz.izquierda.contenido.should.equal(tokens.get(1))
+          should.assert(raiz.izquierda.izquierda == null, 'La rama izquierda no es null')
+          should.assert(raiz.izquierda.derecha == null, 'La rama derecha no es null')
+
+          raiz.derecha.contenido.should.equal(tokens.get(3))
+          should.assert(raiz.derecha.izquierda == null, 'La rama izquierda no es null')
+          should.assert(raiz.derecha.derecha == null, 'La rama derecha no es null')
+      })
+
+      it('detecta un parentesis abierto', () => {
+          const tokens = (leer('(a&b').resultado as ExpresionLeida).tokens
+          const arbol_maybe = arbol(tokens, {error: false, resultado: new Nodo<Token>()})
+
+          arbol_maybe.error.should.equal(true)
+
+          arbol_maybe.resultado.should.deepEqual({nombre: 'ParentesisAbierto', pos: 0})
+      })
+
+      it('detecta un parentesis extraviado', () => {
+          const tokens = (leer('a&b)').resultado as ExpresionLeida).tokens
+          const arbol_maybe = arbol(tokens, {error: false, resultado: new Nodo<Token>()})
+
+          arbol_maybe.error.should.equal(true)
+
+          arbol_maybe.resultado.should.deepEqual({nombre: 'ParentesisExtraviado', pos: 3})
+      })
+  })
+
+  describe('rpn', () => {
+      it('funciona', () => {
+          const exp_maybe = rpn(leer('(a&b)'))
+
+          exp_maybe.error.should.equal(false)
+
+          const exp = exp_maybe.resultado as List<Token>
+
+          exp.get(0).should.deepEqual({nombre: 'a', texto: 'a', tipo: 'var', pos: 1})
+          exp.get(0).should.deepEqual({nombre: 'b', texto: 'b', tipo: 'var', pos: 3})
+          exp.get(0).should.deepEqual({nombre: 'conjuncion', texto: '&', tipo: 'conectivo', pos: 2})
+      })
+
+      it('devuelve el error encontrado', () => {
+          const exp_maybe = rpn(leer('(a&b'))
+
+          exp_maybe.error.should.equal(true)
+          exp_maybe.resultado.should.deepEqual({nombre: 'ParentesisAbierto', pos: 0})
+      })
+  })
 })
